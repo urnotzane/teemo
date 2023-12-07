f9779a135e3fc5faa34b7b6b0fead0bf587b6867
urnotzane <kaylezang@tencent.com>
2023-12-07 15:20:54 +0800

    feat: 新增LCU请求方法

--- examples/teemo.rs
+++ examples/teemo.rs
@@ -1,7 +1,11 @@
 use teemo::Teemo;
 
-fn main() {
+#[tokio::main]
+async fn main() {
   let mut teemo = Teemo::new();
-
   teemo.start();
+
+  // 发送LCU请求
+  let summoner = teemo.request("GET", "lol-summoner/v1/current-summoner", None).await;
+  println!("{:#?}", summoner.unwrap());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use url::Url;
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
+use serde_json::Value;
+use reqwest::Method;
 
 mod utils;
 
+#[derive(Debug)]
 pub struct Teemo {
     app_token: String,
     app_port: i32,
@@ -18,7 +22,7 @@ impl Teemo {
             app_token: String::from(""),
             app_port: 0,
             url: Url::parse("https://127.0.0.1").unwrap(),
-            ws_url: Url::parse("https://127.0.0.1").unwrap(),
+            ws_url: Url::parse("wss://127.0.0.1").unwrap(),
             closed: false,
         }
     }
@@ -56,8 +60,44 @@ impl Teemo {
             self.ws_url =
                 Url::parse(&("wss://127.0.0.1:".to_string() + &self.app_port.to_string())).unwrap();
             
-            println!("Teemo has finished initializing.");
+            println!("Teemo has finished initializing.LCU is running on {}", self.url);
+            println!("LCU is running on {}, token: {}", self.url, self.app_token);
             break;
         }
+    }
+
+    /// 用来发送LCU请求
+    pub async fn request(
+        &self,
+        method: &str,
+        url: &str,
+        data: Option<HashMap<String, Value>>,
+    ) -> Result<HashMap<String, Value>, serde_json::Error> {
+        // if self.app_token.len() < 1 {
+        //     return Ok(HashMap::new());
+        // }
+
+        let method_byte = method.as_bytes();
+        let client: reqwest::Client = reqwest::Client::builder()
+            .danger_accept_invalid_certs(true)
+            .no_proxy()
+            .build()
+            .unwrap();
+    
+        let response = client
+            .request(
+                Method::from_bytes(method_byte).unwrap(),
+                format!("{}{}", self.url, url),
+            )
+            .basic_auth("riot", Some(&self.app_token))
+            .json(&data)
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+        
+        Ok(serde_json::from_str(&response).unwrap())
     }
 }
